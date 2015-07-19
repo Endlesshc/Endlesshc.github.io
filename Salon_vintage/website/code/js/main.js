5c889ebbaa67c42c0c04fab33b9b654654902e5b
Kaven Martineau <asylumx76@gmail.com>
2015-07-19 15:42:14 -0400

    Added form and animations

--- Salon_vintage/website/code/js/main.js
+++ Salon_vintage/website/code/js/main.js
@@ -8,36 +8,6 @@ $(document).scroll(function () {
    	}
 });
 
-$(function (){
-  $('.first-arrow').click(function(){
-    $('.first-arrow').hide();
-    $('.second-arrow').show();
-  });
-});
-$(function (){
-  $('.second-arrow').click(function(){
-    $('.second-arrow').hide();
-    $('.third-arrow').show();
-  });
-});
-
-
-$(function (){
-  $('.third-arrow').click(function(){
-    $('.third-arrow').fadeOut(600);
-    
-  });
-});
-
-$(function (){
-  $('.back-to-top-arrow').click(function(){
-    $('.back-to-top-arrow').hide();
-    $('.first-arrow').show();
-  });
-});
-
-
-
   $(window).scroll(function() {
 
    if ($(this).scrollTop()>3200)
@@ -53,9 +23,113 @@ $(function (){
 
 
 $(function(){
+  $('.tattoo').click(function(){
+      $('.haircuts, .nails').hide('slide');
+      $('.tattoos').slideToggle();
+    });
+});
+$(function(){
+  $('.haircut').click(function(){
+      $('.tattoos, .nails').hide('slide');
+      $('.haircuts').slideToggle();
+    });
+});
+$(function(){
+  $('.nail').click(function(){
+      $('.haircuts, .tattoos').hide('slide');
+      $('.nails').slideToggle();
+    });
+});
+
+$(function(){
 	$('.hamburger-menu').click(function(){
     	$('.mobile-sub-menu').slideToggle();
     	$('.hamburger-menu').toggleClass('opened-mobile-menu');
       $('.mobile-menu-title').toggleClass('hidden');
     });
 });
+
+
+
+
+
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+( function( window ) {
+
+'use strict';
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
