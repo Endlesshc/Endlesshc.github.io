817ed0ba4dc8a85d56afc11a6e0bb4776fab31ea
Kaven Martineau <asylumx76@gmail.com>
2015-08-04 23:07:48 -0400

    meeeerrrgeeeee

--- Vintage/plugin/FlowGallery/Demos/js/init.js
+++
@@ -1,8 +0,0 @@
-(function($){
-
-    $('.button-collapse').sideNav();
-    $('.collapsible').collapsible();
-    $('.parallax').parallax();
-    $('.dropdown-button').dropdown({hover: false});
-
-})(jQuery); // end of jQuery name space
